4d631550f9020d628ea48a4a89a257424e69d631
Calvin Carter <calvincarter@pingidentity.com>
2021-06-20 22:38:56 -0500

    fix ssl issue w/in heroku

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const { Client } = require("pg");
 
+const sslConfig = process.env.DATABASE_URL ? {rejectUnauthorized: false} : undefined;
+
 const db = new Client({
-  connectionString: process.env.DATABASE_URL || "postgresql:///rickllow"
+  connectionString: process.env.DATABASE_URL || "postgresql:///rickllow",
+  ssl: sslConfig
 });
 
 db.connect();
