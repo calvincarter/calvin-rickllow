b23d712d4982af594d2d95a911c00ef5b606ba7e
Calvin Carter <calvincarter@pingidentity.com>
2021-06-29 09:42:54 -0500

    remove generic 500 error handling

--- app.js
+++ app.js
@@ -21,15 +21,4 @@ app.use(function(req, res) {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
-/** Generic error handler; anything unhandled goes here. */
-app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
-
-  return res.status(status).json({
-    error: { message, status },
-  });
-});
-
 module.exports = app;
